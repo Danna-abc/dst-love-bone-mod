b76248b077c72db9bd256f3ac7aaf77ce848d2d5
Danna-abc <1659307236@qq.com>
2026-07-09 00:05:43 +0800

    modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,10 @@
+名称 = "往生奔赴骨片｜情人节双人羁绊传送"
+description = "双人联机专属信物。敲碎阵亡玩家的骨架，必定获得往生骨片，52%概率开出奔赴骨片。\n往生骨片：一次性传送至队友陨落之地（机制对标传送木牌）\n奔赴骨片：无消耗反复传送，实时跟随队友当前位置"
+作者 = “你的工坊ID”
+版本 = "1.0"
+api_version = 10
+all_clients_require_mod = true
+client_only_mod = false
+dst_mod = true
+
+configuration_options = {}
