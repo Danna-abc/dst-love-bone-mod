23eedca0056e1c97850edc141a23832b3c20530b
Danna-abc <1659307236@qq.com>
2026-07-09 16:52:50 +0800

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,8 +1,9 @@
-name = "Past Life Bone Chip | Valentine Double Teleport Bond"
+name = "奔赴往生骨片"
 description = "双人联机专属信物。敲碎阵亡玩家的骨架，必定获得往生骨片，52%概率开出奔赴骨片。\n往生骨片：一次性传送至队友陨落之地（机制对标传送木牌）\n奔赴骨片：无消耗反复传送，实时跟随队友当前位置"
-author = "MySelf"
+author = "Alan"
 version = "1.0"
 api_version = 10
+dst_compatible = true   
 all_clients_require_mod = true
 client_only_mod = false
 dst_mod = true
