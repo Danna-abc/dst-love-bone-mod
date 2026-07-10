4f6fc8d8d413bdc369a2fef0e24316254c36f5bc
Danna-abc <1659307236@qq.com>
2026-07-10 17:00:09 +0800

    Add files via upload

---
+++ languages/chs.lua
@@ -0,0 +1,14 @@
+STRINGS.BONE = {
+    NO_POS = "这块骨片没有记录死亡地点。",
+    NOT_YOUR = "这不是你的骨片。",
+    OCEAN_FAIL = "目标地是海洋，无法传送。",
+    TELE_SUCC = "传送成功。",
+    NO_BIND = "这块骨片没有绑定任何人。",
+    OFFLINE = " 不在线。",
+    GHOST = " 是鬼魂。",
+    OCEAN_BIND = "目标在海洋，无法传送。",
+    BIND_SUCC = "奔赴成功。",
+    BAG_FULL = "背包已满！",
+    BIND_CREATE = "你和%s建立了羁绊。",
+    BIND_YOU = "%s和你建立了羁绊。"
+}
