46a2f192de1f14d5904b1aade1bb37fa6625f3ec
Alan <1659307236@qq.com>
2026-07-18 15:18:47 +0800

    Add files via upload

---
+++ scripts/strings/chs.lua
@@ -0,0 +1,28 @@
+GLOBAL.STRINGS.NAMES.WANGSHENG_BONE = "往生骨片"
+GLOBAL.STRINGS.NAMES.BENFU_BONE = "奔赴羁绊骨片"
+
+GLOBAL.STRINGS.ACTIONS.USE_WANGSHENG = "使用往生骨片"
+GLOBAL.STRINGS.ACTIONS.USE_BENFU = "使用奔赴羁绊骨片"
+GLOBAL.STRINGS.ACTIONS.BIND_BENFU = "与对方建立羁绊"
+
+GLOBAL.STRINGS.BONE = {
+    NO_POS = "这块骨片没有记录死亡地点。",
+    NOT_YOUR = "这不是你的骨片。",
+    OCEAN_FAIL = "目标地是海洋，无法传送。",
+    TELE_SUCC = "传送成功。",
+    NO_BIND = "这块骨片没有绑定任何人。",
+    OFFLINE = " 不在线。",
+    GHOST = " 是鬼魂。",
+    OCEAN_BIND = "目标在海洋，无法传送。",
+    BIND_SUCC = "奔赴成功。",
+    BAG_FULL = "背包已满！",
+    BIND_CREATE = "你和%s建立了羁绊。",
+    BIND_YOU = "%s和你建立了羁绊。",
+}
+
+GLOBAL.STRINGS.ITEM = {
+    WANG_EMPTY = "一枚记录亡者坐标的骨片，右键回到陨落之地。",
+    WANG_DESC = "前往%s陨落的地点，使用后消散。",
+    BENFU_EMPTY = "双人羁绊骨片，还未与任何人绑定。",
+    BENFU_DESC = "与%s绑定的羁绊骨片，右键可无限次奔赴对方身边。",
+}
