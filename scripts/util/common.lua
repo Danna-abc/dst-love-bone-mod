eb23cb1ffa84735815b89112a62aeab45f0f47df
Alan <1659307236@qq.com>
2026-07-25 17:33:51 +0800

    Add files via upload

---
+++ scripts/util/common.lua
@@ -0,0 +1,44 @@
+local GLOBAL = GLOBAL
+
+local M = {}
+
+function M.IsGhost(player)
+    return player:HasTag("ghost")
+end
+
+function M.IsOceanAtPoint(x, z)
+    return GLOBAL.TheWorld.Map:IsOceanAtPoint(x, 0, z)
+end
+
+function M.GetPlayerByUserid(uid)
+    for _, p in ipairs(GLOBAL.AllPlayers) do
+        if p.userid == uid then
+            return p
+        end
+    end
+    return nil
+end
+
+function M.HasFullSpace(plr)
+    local inv = plr.components.inventory
+    if not inv then return false end
+    for i = 1, 4 do
+        if not inv:GetItemInSlot(i) then return true end
+    end
+    return inv:NumItems() < inv.maxslots
+end
+
+function M.SafeGiveBone(plr, bone)
+    if not bone then return end
+    local inv = plr.components.inventory
+    if not inv then return end
+    for i = 1, 4 do
+        if not inv:GetItemInSlot(i) then
+            inv:SetItemInSlot(i, bone)
+            return
+        end
+    end
+    inv:GiveItem(bone)
+end
+
+return M
