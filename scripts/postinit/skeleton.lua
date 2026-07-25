34b4b13a85230c6d327cb90ba4791122a2ef3090
Alan <1659307236@qq.com>
2026-07-25 18:38:54 +0800

    Delete scripts/postinit/skeleton.lua

--- scripts/postinit/skeleton.lua
+++
@@ -1,9 +0,0 @@
-local GLOBAL = GLOBAL
-local AddPrefabPostInit = GLOBAL.AddPrefabPostInit
-local DeathManager = modimport("scripts/manager/deathmanager.lua")
-
-AddPrefabPostInit("skeleton", function(inst)
-    inst:ListenForEvent("entityremove", function()
-        DeathManager.SpawnBones(inst)
-    end)
-end)
