2a9f328156291db88061b8d7c5e40fe833c316b1
Alan <1659307236@qq.com>
2026-07-25 18:40:15 +0800

    Delete scripts/postinit/shallowgrave.lua

--- scripts/postinit/shallowgrave.lua
+++
@@ -1,9 +0,0 @@
-local GLOBAL = GLOBAL
-local AddPrefabPostInit = GLOBAL.AddPrefabPostInit
-local DeathManager = modimport("scripts/manager/deathmanager.lua")
-
-AddPrefabPostInit("shallowgrave", function(inst)
-    inst:ListenForEvent("workfinished", function()
-        DeathManager.SpawnBones(inst)
-    end)
-end)
